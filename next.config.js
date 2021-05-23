06fca0e759cebc6b05d4e1b9ff98242b08b7dcc1
AlbertoPimentel <albertopimentel04@gmail.com>
2021-05-22 22:34:56 -0500

    public_key en config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ module.exports = {
     domains: ["links.papareact.com", "fakestoreapi.com"],
   },
   env: {
-    stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
+    stripe_public_key:
+      process.env.STRIPE_PUBLIC_KEY ||
+      "pk_test_51ItzJLJYvLIBvUAlS2lDxwXpvpcWx7NGS1Jq73XU2LUfRReoFBl3FsKcpSN8pHXkKZdHyTYjaxArsgqHxpG0w68D009J9ZsrpF",
   },
 };
