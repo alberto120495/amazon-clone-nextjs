967e25fcd2124ea11509a0eb91349f1bebd96766
AlbertoPimentel <albertopimentel04@gmail.com>
2021-05-22 22:45:48 -0500

    public key in checkout

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ module.exports = {
     domains: ["links.papareact.com", "fakestoreapi.com"],
   },
   env: {
-    stripe_public_key:
-      process.env.STRIPE_PUBLIC_KEY ||
-      "pk_test_51ItzJLJYvLIBvUAlS2lDxwXpvpcWx7NGS1Jq73XU2LUfRReoFBl3FsKcpSN8pHXkKZdHyTYjaxArsgqHxpG0w68D009J9ZsrpF",
+    stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
   },
 };
